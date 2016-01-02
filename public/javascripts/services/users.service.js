f0f570ba073a9a3a51686a47ed9a7f1c3e080263
Alejandro Gutierrez <alexone22@Alexs-MacBook-Pro.local>
2016-01-02 14:45:46 -0800

    fixed bower errors, users have full crud features

--- public/javascripts/services/users.service.js
+++ public/javascripts/services/users.service.js
@@ -2,9 +2,19 @@
   'use strict';
 
   angular.module('app')
-    .service('Users', function($http){
+    .service('Users', function($http, $uibModal, $rootScope){
       var vm = this;
       vm.users = [];
+
+      /**
+       * Find a user in the users list.
+       *
+       * @param userId
+       * @returns {*}
+       */
+      vm.find = function find(userId) {
+        return _.find(vm.users, {_id: userId});
+      };
 
 
       /**
@@ -23,6 +33,59 @@
             });
 
             return vm.users;
+          });
+      };
+
+      /* Add a user with Modal */
+      vm.add = function(){
+        vm.openUserModal().then(function(user){
+          return $http.post('/users', user)
+            .then(function(res){
+              vm.users.push(res.data);
+            }, function (err) {
+            console.error(err);
+          });
+        });
+      };
+
+      /* Editing a User through modal*/
+      vm.edit = function(user){
+        vm.scope = $rootScope.$new();
+        vm.scope.user = user;
+        vm.openUserModal().then(function(newUser){
+          return $http.put('/users/' + newUser._id, newUser)
+            .then(function(res){
+              var _user = vm.find(newUser._id);
+              _.merge(_user, newUser);
+            }, function(err){
+              console.error(err);
+            });
+        });
+      };
+
+      /* Modal to add or edit a User*/
+      vm.openUserModal = function(){
+        return $uibModal.open({
+                controller: 'UserModalController',
+                controllerAs: 'userModal',
+                templateUrl: 'partials/users/user-modal-form.html',
+                scope: vm.scope
+              }).result;
+      };
+
+
+      /**
+       * Delete a user.
+       *
+       * @param userId
+       * @returns {*}
+       */
+      vm.delete = function(userId){
+        return $http.delete('/users/' + userId)
+          .then(function(res){
+              _.remove(vm.users, {_id: userId});
+          }, function(err){
+            console.error(err);
           });
       };
 
